ba5e4749a5bd2a6fd1c2dc9792301df324e926c4
ChaN <joshuakke355@gmail.com>
2026-08-03 22:33:56 +0800

    上传文件的配置文件编写

---
+++ backend/src/main/java/com/aistudy/backend/config/FileUploadConfig.java
@@ -0,0 +1,14 @@
+﻿package com.aistudy.backend.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "file.upload")
+public class FileUploadConfig {
+    private String path;
+    private String allowedTypes;
+    private String maxSize;
+}
